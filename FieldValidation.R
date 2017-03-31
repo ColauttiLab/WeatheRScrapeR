56ad6d6896e0f98e3f599bbbe4dd0f6576acafc6
yihanwu <yihan.wu@queensu.ca>
2017-03-31 14:08:49 -0400

    Put in season metric calculation loop. The script produces a file with GD and other metrics for each field population.

--- FieldValidation.R
+++ FieldValidation.R
@@ -2,6 +2,10 @@ library(ggplot2)
 library(dplyr)
 library(reshape2)
 library(lme4)
+library(fields) 
+library(zoo) 
+library(tibble)
+library(FSA)
 source("geodist.R")
 
 ##Data Import
@@ -9,70 +13,164 @@ PhenolAllData<-read.csv("PhenolAllData.csv", header=T, stringsAsFactors=FALSE)
 PhenolAllData<-subset(PhenolAllData, Year >=1960)
 FieldData<-read.csv("FieldPhenology.csv", header=T, stringsAsFactors = FALSE)
 
+
+StnData<-read.csv("WeatherRawData/NOAAMontagueStation.csv", header=T)
+
+
 ##Data Setup
 names<-names(FieldData)
 names[1]<-"Pop_Code"
 names[10]<-"yday"
+names[8]<-"calculatedGDD"
 names(FieldData)<-names
 FieldData$Year<-2004
+FieldData$yday<-round(FieldData$yday) ##rounded in order to make GD and GDD calcuations
+
+
+write.csv(FieldData, "FieldPopulation.csv", row.names=FALSE)
 ###subset to range around field populations
 ValidHerb<-PhenolAllData[(PhenolAllData$Latitude < 49) & (PhenolAllData$Longitude > -85), ]
 ValidHerb<-ValidHerb[(ValidHerb$Latitude > 38) & (ValidHerb$Longitude < -74), ]
 
 
 
-##
 
-###scrap
-#Binning into populations around the field populations
-
-binx<-1 ## long bin size in degrees
-biny<-1## lat bin size in degrees
-
-PopList<-{}
-ClosePop<-{}
-SMin<-3
-SMax <-10
-for (i in 1:nrow(FieldData)){
-  ##find all samples within range of binx and bin y
-  ClosePop<- PopData[PopData$Longitude >= FieldData$Longitude[i]-binx & PopData$Longitude <= FieldData$Longitude[i]+binx & PopData$Latitude >= FieldData$Latitude[i]-biny & PopData$Latitude <= FieldData$Latitude[i]+binx,]
-  if (nrow(ClosePop >= SMin)) {
-    for(j in 1:nrow(ClosePop)){ # Cycle through each station
-      ClosePop$Dist[j]<-geodist(c(ClosePop$Latitude[j],ClosePop$Longitude[j],PopData$Latitude[i],PopData$Longitude[i]))   
+Cntr<-0
+for(year in (2003:2005)){ 
+  
+  # Open file with GD data
+  GDFilePath<-paste0("WeatherRawData/NOAAStnsMontague",year,".csv") 
+  GDData<-read.csv(GDFilePath)
+  for(Pop in FieldData$Pop_Code[FieldData$Year==year]){ # Cycle through pop_codes sampled in same year as GD data
+    Cntr<-Cntr+1
+    # Find names of nearby stations
+    LocStns<-paste(unique(StnData$StationID[StnData$Pop_Code==Pop & StnData$Measure=="TMAX"]))
+    # Subset GDData for stations of interest from Jan 1 to day of sampling
+    PopGDData<-GDData[GDData$StationID %in% LocStns,]
+    
+    
+    ## Remove stations with >10% missing data
+    LocStns<-names(summary(PopGDData$StationID[!is.na(PopGDData$GDeg)])[summary(PopGDData$StationID[!is.na(PopGDData$GDeg)])>365*0.90])
+    if(length(LocStns)<20){# If less than 20 stations, only remove stations with >30% missing data
+      LocStns<-names(summary(PopGDData$StationID[!is.na(PopGDData$GDeg)])[summary(PopGDData$StationID[!is.na(PopGDData$GDeg)])>365*0.70])
     }
+    if(length(LocStns)>20){
+      # Sort remaining stations by distance
+      LocStns<-unique(StnData[StnData$Pop_Code==Pop & StnData$StationID %in% LocStns,c("StationID","Dist")])
+      # Keep closest 20 stations 
+      LocStns<-paste(LocStns$StationID[order(LocStns$Dist)][1:20])
+    }
+    # Re-Subset GDData now that we have screened for missing data and distance
+    PopGDData<-GDData[GDData$StationID %in% LocStns,]
+    # Check for problems with climate data using mean and var to identify outliers
+    OutTest<-aggregate(PopGDData$TMAX,by=list(PopGDData$StationID),FUN=c("mean"),na.rm=T)
+    names(OutTest)<-c("Stn","mean")
+    OutTest$var<-aggregate(PopGDData$TMAX,by=list(PopGDData$StationID),FUN=c("var"),na.rm=T)$x
+    OutTest$mean<-abs((OutTest$mean-median(OutTest$mean))/median(OutTest$mean))<0.3
+    OutTest$var<-abs((OutTest$var-median(OutTest$var))/median(OutTest$var))<0.3
+    ##Update LocStns with Stations that are kept
+    LocStns <- OutTest$Stn[OutTest$var & OutTest$mean]
+    # Re-subset to remove outlier stations
+    PopGDData<-GDData[GDData$StationID %in% LocStns,]
+    
+    # Make data frame to collect GDD, lat and long for each station
+    GeoDat<-unique(StnData[StnData$StationID %in% LocStns,1:3])
+    GeoDat$GD<-NA ##Tota Growing Season Length (in Days)
+    GeoDat$GDs<-NA ##Growing Seaon Length to collection (in Days)
+    GeoDat$GDD<-NA ##Total Growing Degree Days over season (in Growing Degrees)
+    GeoDat$GDDs<-NA ##Growing Degree Days cumulative to time of collection (in Growing Degrees)
+    GeoDat$meanGDeg<-NA ##mean of growing degrees per day / total season length (GDD/GD)
+    GeoDat$varGDeg<-NA ##variance of growing degrees per day over the season 
+    GeoDat$skewGDeg<-NA ##skew of growing degrees per day over the season 
+    GeoDat$kurtGDeg<-NA ##variance of growing degrees per day over the season 
+    
+    ##Calculate GD, GDs, GDD, GDDs values for each station
+    for (Stn in LocStns) { 
+      ###
+      yday<-as.numeric(FieldData$yday[FieldData$Pop_Code==Pop])
+      test<-GDData[grep(Stn, GDData$StationID), ]   
+      summary(test$GDeg) ## if NA exist for GDeg, run zoo
+      if(anyNA(test$GDeg)){
+        test$GDeg <-na.approx(test$GDeg, rule=2) ##rule 2 to deal with NA values at start and finish, not a good way because it repeats values
+      }
+      test$Indicator <-FALSE
+      ####Set days with positive GDD to true
+      test$Indicator <- ifelse(test$GDeg > 0, TRUE, test$Indicator)
+      ## Give sequences of growing degree days and non-growing days
+      rletest<-rle(test$Indicator)
+      ##put rle results into table
+      length<-rletest$lengths
+      value<-rletest$values
+      df<-data.frame(length, value)
+      df<- df %>% rownames_to_column()
+      ##find values
+      intervals<-df[which(df$length >= 10 & df$value ==TRUE),]
+      ## row numbers for the beginning(min) and end(max) of the growing season
+      rowdates<-c(min(as.numeric(intervals$rowname)), max(as.numeric(intervals$rowname)))  ## first value is beginning of season, second value is end of season
+      df$cumDayminus<-pcumsum(df$length) ### these values are for beginning of season
+      df$cumDay<-cumsum(df$length) ### these values are used for end of season
+      begin <- as.numeric(df$cumDayminus[as.integer(rowdates[1])]) +1 ##first day of season, need one day added because cumDayminus means it starts at day before the actual start of season
+      end <- as.numeric(df$cumDay[as.integer(rowdates[2])]) ##last day of season
+      
+      
+      #Calculations for season length, and season to collection, moments of distribution
+      GD <- (end - begin) + 1  ##season length, need +1 so start of season is included
+      GDs <- yday - begin + 1 ##length of season to collection, need +1 so start of season is included
+      GDD <- sum(test$GDeg[begin:end]) ## GDD for the entire season
+      GDDs <- sum(test$GDeg[begin:yday]) ##GDD from start of season to collection
+      
+      test <- test[c(begin:end),] ##subset data to only growing season
+      meanGDeg <- mean(test$GDeg) ##mean of growing degrees per day
+      varGDeg <- sum((test$GDeg - meanGDeg)^2)/GD ## var of growing degrees per day for growing season, no adjustion for sample size
+      skewGDeg <- ((sum((test$GDeg - meanGDeg)^3))/GD) /(varGDeg)^(3/2) ##skewness, Fisher-Pearson (not adjusted for sample size)
+      kurtGDeg <- ((sum((test$GDeg - meanGDeg)^4))/GD) /(varGDeg)^(4/2) -3 ##excess kurtosis for univariate data, 
+      
+      
+      
+      ##Data Values to Put into GeoDat (all stations for each pop)
+      GeoDat$GD[GeoDat$StationID==Stn] <- GD
+      GeoDat$GDs[GeoDat$StationID==Stn] <- GDs
+      GeoDat$GDD[GeoDat$StationID==Stn] <- GDD
+      GeoDat$GDDs[GeoDat$StationID==Stn] <- GDDs
+      GeoDat$meanGDeg[GeoDat$StationID==Stn] <- meanGDeg
+      GeoDat$varGDeg[GeoDat$StationID==Stn] <- varGDeg
+      GeoDat$skewGDeg[GeoDat$StationID==Stn] <- skewGDeg
+      GeoDat$kurtGDeg[GeoDat$StationID==Stn] <- kurtGDeg
+    }
+    
+    
+    # Spatial interpolation of GDD
+    if(nrow(GeoDat)>=5){ # Only interpolate if at least 10 stations available
+      # Interpolate GDD for Pop
+      ### ALSO CHANGE THIS TO INTERPOLATE FOR GDD, GD, GDDs, GDs
+      tpsGD<-Tps(x=GeoDat[,c("Longitude","Latitude")],Y=GeoDat$GD,scale.type="unscaled", lon.lat=T)
+      tpsGDs<-Tps(x=GeoDat[,c("Longitude","Latitude")],Y=GeoDat$GDs,scale.type="unscaled", lon.lat=T)
+      tpsGDD<-Tps(x=GeoDat[,c("Longitude","Latitude")],Y=GeoDat$GDD,scale.type="unscaled", lon.lat=T) # see details in ?Tps  
+      tpsGDDs<-Tps(x=GeoDat[,c("Longitude","Latitude")],Y=GeoDat$GDDs,scale.type="unscaled", lon.lat=T)
+      tpsmeanGDeg<-Tps(x=GeoDat[,c("Longitude","Latitude")],Y=GeoDat$meanGDeg,scale.type="unscaled", lon.lat=T)
+      tpsvarGDeg<-Tps(x=GeoDat[,c("Longitude","Latitude")],Y=GeoDat$varGDeg,scale.type="unscaled", lon.lat=T)
+      tpsskewGDeg<-Tps(x=GeoDat[,c("Longitude","Latitude")],Y=GeoDat$skewGDeg,scale.type="unscaled", lon.lat=T)
+      tpskurtGDeg<-Tps(x=GeoDat[,c("Longitude","Latitude")],Y=GeoDat$kurtGDeg,scale.type="unscaled", lon.lat=T)
+      
+      FieldData$GD[FieldData$Pop_Code==Pop]<-predict(tpsGD,x=FieldData[FieldData$Pop_Code==Pop,c("Longitude","Latitude")],scale.type="unscaled")
+      FieldData$GDs[FieldData$Pop_Code==Pop]<-predict(tpsGDs,x=FieldData[FieldData$Pop_Code==Pop,c("Longitude","Latitude")],scale.type="unscaled")
+      FieldData$GDD[FieldData$Pop_Code==Pop]<-predict(tpsGDD,x=FieldData[FieldData$Pop_Code==Pop,c("Longitude","Latitude")],scale.type="unscaled")      
+      FieldData$GDDs[FieldData$Pop_Code==Pop]<-predict(tpsGDDs,x=FieldData[FieldData$Pop_Code==Pop,c("Longitude","Latitude")],scale.type="unscaled")   
+      FieldData$meanGDeg[FieldData$Pop_Code==Pop]<-predict(tpsmeanGDeg,x=FieldData[FieldData$Pop_Code==Pop,c("Longitude","Latitude")],scale.type="unscaled")
+      FieldData$varGDeg[FieldData$Pop_Code==Pop]<-predict(tpsvarGDeg,x=FieldData[FieldData$Pop_Code==Pop,c("Longitude","Latitude")],scale.type="unscaled")
+      FieldData$skewGDeg[FieldData$Pop_Code==Pop]<-predict(tpsskewGDeg,x=FieldData[FieldData$Pop_Code==Pop,c("Longitude","Latitude")],scale.type="unscaled")
+      FieldData$kurtGDeg[FieldData$Pop_Code==Pop]<-predict(tpskurtGDeg,x=FieldData[FieldData$Pop_Code==Pop,c("Longitude","Latitude")],scale.type="unscaled")
+      FieldData$numStns[FieldData$Pop_Code==Pop]<-nrow(GeoDat)
+      # SAVE output
+      write.csv(FieldData,"FieldData_wGDDtest.csv",row.names=F)
+    }
+    cat("***************\nIteration ",Cntr," of",length(unique(FieldData$Pop_Code)),"\nYear: ",year,"\nPop: ",Pop,"\n",Sys.time(),"seconds","\nGDD: ",FieldData$GDD[FieldData$Pop_Code==Pop],"\nGDays: ",FieldData$GDays[FieldData$Pop_Code==Pop],"\n***************")
+    yday<-LocStns<-PopGDData<-GeoDat<-tps<-NA # clean up for next iteration of pop
   }
-    ##only take the Smax closest stations
-    if (nrow(ClosePop)>= SMax) {
-    ClosePop<-ClosePop[order(ClosePop$Dist),][1:SMax,]
-    }
-  ClosePop<-cbind(ClosePop[,c(1,10,11,14,34)], FieldData$Population[i], FieldData$weighted.mean.date.of.peak.flowering[i], FieldData$weighted.average...days.after.growing.season.to.flowering[i], FieldData$Latitude[i], FieldData$Longitude[i])
-  PopList<-rbind(PopList, ClosePop)
+  GDData<-GDFilePath<-NA # Clean-up for next iteration of year
 }
 
-##rename data set
-names(PopList)<-c("Pop_Code", "Latitude", "Longitude", "Pop_GD", "Pop_phind", "FieldPop", "FieldTotalFlower", "FlowerGFlower", "FieldLatitude", "FieldLongitude")
 
-
-##aggregate phind by field populations to "estimate" phind for the population
-agField<-aggregate(PopList, by=list(PopList$FieldPop), FUN=mean)
-
-ggplot(data=agField, aes(x = Pop_phind, y = FieldTotalFlower)) + geom_point()+geom_smooth()
-
-ggplot(data=agField, aes(x = Pop_phind, y = FlowerGFlower)) + geom_point()+geom_smooth()
-
-
-fit1<-lm(agField$Pop_phind~agField$FieldTotalFlower)
-summary(fit1)
-cor(agField$Pop_phind,agField$FieldTotalFlower)
-
-fit2<-lm(agField$Pop_phind~agField$FlowerGFlower)
-summary(fit2)
-
-cor(agField$Pop_phind,agField$FlowerGFlower)
-##plotting every sample to field flowering
-
-
-ggplot(data=PopList, aes(x = Pop_phind, y = FieldTotalFlower)) + geom_point()+geom_smooth()
 
 
 
